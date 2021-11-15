194312ba409b9a229d7af1069439868665287e71
Mollycodd1e <selik7@mail.ru>
2021-11-16 00:04:19 +0300

    Добавляет открытие/закрытие меню на мобилке

--- project/src/components/header/header.jsx
+++ project/src/components/header/header.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { PAGES } from '../../const';
+import React, {useState} from 'react';
+import {PAGES} from '../../const';
 import Logo from '../logo/logo';
 import PagesList from '../pages-list/pages-list';
 import {useDispatch} from "react-redux";
@@ -9,14 +9,16 @@ function Header() {
 
   const dispatch = useDispatch();
 
+  const [isClick, setClick] = useState(false);
+
   return (
     <header className="main-header">
-      <div className="main-header__wrapper">
+      <div className={`main-header__wrapper ${isClick ? 'main-header__wrapper--opened' : 'main-header__wrapper--closed'}`}>
         <div className="main-header__logo" onClick={() => dispatch(changePage(PAGES[0].page))}>
           <Logo />
         </div>
         <PagesList />
-        <div className="main-header__button"></div>
+        <div className="main-header__button" onClick={() => {isClick ? setClick(false) : setClick(true)}}></div>
       </div>
     </header>
   );
